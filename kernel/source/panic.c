97049d35327b72ded7150ca9d7755a1acfce6401
BlurrySquire <114335081+BlurrySquire@users.noreply.github.com>
2025-07-31 16:12:18 +0100

    Don't print to the console as the console could also crash during a panic.

--- kernel/source/panic.c
+++ kernel/source/panic.c
@@ -6,12 +6,6 @@
 #include <stdbool.h>
 
 void kernel_panic(const char* message) {
-    console_set_colour(CONSOLE_COLOUR_WHITE, CONSOLE_COLOUR_RED);
-
-    console_print("PANIC: ");
-    console_print(message);
-    console_print("\n");
-
     serial_send_string("PANIC: ");
     serial_send_string(message); 
     serial_send_string("\n");
