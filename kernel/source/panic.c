535d83332ab750857775dd98f9465d8d2537054e
BlurrySquire <114335081+BlurrySquire@users.noreply.github.com>
2025-07-31 16:32:35 +0100

    Allow for panic outputting to console, but only once to avoid an infinite loop of panics.

--- kernel/source/panic.c
+++ kernel/source/panic.c
@@ -3,11 +3,36 @@
 #include "serial.h"
 #include "console.h"
 
+#include <stdint.h>
 #include <stdbool.h>
 
+/*
+    It is really rare for serial to
+    cause a kernel panic, so we can
+    output to serial for every panic.
+
+    However, the console is more likely
+    to cause a kernel panic, so we
+    will only output to it for the first
+    panic recieved.
+
+    This avoids an infinite loop of panics.
+*/
+
+uint8_t counter = 0;
+
 void kernel_panic(const char* message) {
+    if (counter < 1) {
+        counter++;    
+    
+        console_set_colour(CONSOLE_COLOUR_WHITE, CONSOLE_COLOUR_RED);
+        console_print("PANIC: ");
+        console_print(message);
+        console_print("\n");
+    }
+    
     serial_send_string("PANIC: ");
-    serial_send_string(message); 
+    serial_send_string(message);
     serial_send_string("\n");
 
     kernel_hang();
