fd47b4c736b7d6a2fd3001d4d1f89da970f0de07
BlurrySquire <114335081+BlurrySquire@users.noreply.github.com>
2025-08-01 22:24:49 +0100

    Add memory map limine request.

--- kernel/source/limine_requests.h
+++ kernel/source/limine_requests.h
@@ -12,6 +12,12 @@ static volatile struct limine_framebuffer_request framebuffer_request = {
     .revision = 0
 };
 
+__attribute__((used, section(".limine_requests")))
+static volatile struct limine_memmap_request memmap_request = {
+    .id = LIMINE_MEMMAP_REQUEST,
+    .revision = 0
+};
+
 __attribute__((used, section(".limine_requests_start")))
 static volatile LIMINE_REQUESTS_START_MARKER;
 
